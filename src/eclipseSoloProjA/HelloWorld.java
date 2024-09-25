72d1e3b6312bea3405e825546879685d7f2fe22a
nchishty <nchishty@emich.edu>
2024-09-25 14:36:49 -0400

    Added more

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,7 +3,8 @@ package eclipseSoloProjA;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		System.out.println("Hello");
 
 	}
 
