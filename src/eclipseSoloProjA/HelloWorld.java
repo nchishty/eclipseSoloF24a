27486be1d5e57c464e3a63980e87e4150168e3ac
nchishty <nchishty@emich.edu>
2024-09-28 15:20:00 -0400

    Programmer 2 commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 		System.out.println("Hello");
+		System.out.println("Programmer 2 did this");
 
 	}
 
