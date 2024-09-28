8a96c3751e3fd4871ef2ad41fc6be0aea0ea83cb
nchishty <nchishty@emich.edu>
2024-09-28 15:27:47 -0400

    Programmer 1 added

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 		System.out.println("Hello");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this, Programer1 did this");
 
 	}
 
